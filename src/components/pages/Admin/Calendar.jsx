94a3e0be35b5c745ef3b42cd2a053e96bc1c1344
Fuat <fuatbozkurt1@gmail.com>
2019-11-15 23:13:14 +0300

    modal

--- src/components/pages/Admin/Calendar.jsx
+++ src/components/pages/Admin/Calendar.jsx
@@ -8,6 +8,27 @@ import {
   Month,
   Agenda
 } from "@syncfusion/ej2-react-schedule";
+import { L10n } from "@syncfusion/ej2-base";
+
+L10n.load({
+  "en-US": {
+    /*Türkçe karşılıkları  https://github.com/syncfusion/ej2-locale/blob/master/src/tr.json*/
+    schedule: {
+      title: "Başlık",
+      saveButton: "Ekle",
+      cancelButton: "Kapat",
+      deleteButton: "Sil",
+      newEvent: "Randevu Ekle",
+      day: "Gün",
+      week: "Hafta",
+      today: "Bugün",
+      month: "Ay",
+      agenda: "Ajanda",
+      workWeek: "Haftalık Çalışma",
+      monday: "Pazartesi"
+    }
+  }
+});
 
 class Scheduler extends Component {
   state = {};
@@ -18,24 +39,42 @@ class Scheduler extends Component {
         Id: 2,
         Subject: "Paris",
         StartTime: new Date(2019, 9, 23, 10, 0),
-        EndTime: new Date(2019, 9, 23, 12, 30),
-        isReadonly: true,
-        color: "black"
+        EndTime: new Date(2019, 9, 23, 12, 30)
       }
     ];
   }
-  componentDidMount() {}
+  onEventClick(args) {
+    let event = this.scheduleObj.getEventDetails(args.element);
+    // console.log(args.element);
+
+    // console.log(event);
+  }
+  onDataBound() {
+    let events = this.scheduleObj.getCurrentViewEvents();
+    // console.log(events);
+  }
+
+  onActionComplete(s) {
+    if (s.addedRecords) {
+      console.log("deneme", s.addedRecords.Subject);
+    }
+  }
+
   render() {
     return (
       <div className="container">
         <ScheduleComponent
+          ref={t => (this.scheduleObj = t)}
           timezone="Europe/Istanbul"
           width="100%"
           height="100%"
           currentView="Month"
-          selectedDate={new Date(2019, 9, 23)}
+          selectedDate={new Date(Date.now())}
           eventSettings={{ dataSource: this.data }}
           firstDayOfWeek={1}
+          dataBound={this.onDataBound.bind(this)}
+          eventClick={this.onEventClick.bind(this)}
+          actionComplete={this.onActionComplete.bind(this)}
         >
           <Inject services={[Day, Week, Month, Agenda]} />
         </ScheduleComponent>
